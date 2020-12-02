377c31d77d099fa1cfa5d4e55251b176781e6622
Luis Calderon <luis.calderon@anahuac.mx>
2020-12-01 19:13:46 -0600

    Change in import paginator

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.core paginator import Paginator, EmptyPage,\
+from django.core.paginator import Paginator, EmptyPage,\
                                   PageNotAnInteger
 from .models import Post
 
