0a205883a823d81fd93d8ff8b3c6914a9052631a
Luis Gerardo Calderon Contreras <luis.calderon@anahuac.mx>
2020-12-03 14:14:53 -0600

    Similar tags

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,6 +3,7 @@ from django.core.paginator import Paginator, EmptyPage,\
     PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
+from django.db.models import Count
 
 from taggit.models import Tag
 
@@ -64,12 +65,18 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+    post_tags_id = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_id)\
+                                  .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                                 .order_by('-same_tags','-publish')[:4]
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
                    'comments': comments,
                    'new_comment': new_comment,
-                   'comment_form': comment_form})
+                   'comment_form': comment_form,
+                   'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
